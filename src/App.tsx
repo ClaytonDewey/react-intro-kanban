2547def301c341f608b68afc381b01e5d116a7a6
ClaytonDewey <clay@dryadmedia.com>
2025-03-22 12:52:51 -0500

    style: Basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <div className='text-4xl text-red-500'>Hello</div>;
+  const title = 'Do Market Research';
+  const id = 'BUS-1';
+  const points = 5;
+
+  return (
+    <div className='border rounded-lg px-2 m-2 bg-gray-50'>
+      <div className='text-base font-semibold py-2'>{title}</div>
+      <div className='flex gap-4 justify-between py-2 text-gray-500'>
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
