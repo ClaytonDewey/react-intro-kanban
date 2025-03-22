7d91c1840119be20734bd16bb202cca32fcd59b0
ClaytonDewey <clay@dryadmedia.com>
2025-03-22 15:19:03 -0500

    feat: Add priorities & priority icons

--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -1,61 +1,71 @@
 export type Status = 'todo' | 'in-progress' | 'done';
-
+export type Priority = 'low' | 'medium' | 'high';
 export type Task = {
-  title: string;
   id: string;
+  title: string;
   status: Status;
+  priority: Priority;
   points?: number;
 };
 
 export const statuses: Status[] = ['todo', 'in-progress', 'done'];
+export const priorities: Priority[] = ['low', 'medium', 'high'];
 
 export const tasks: Array<Task> = [
   {
-    title: 'Market Research',
     id: 'BUS-1',
+    title: 'Market Research',
     status: 'done',
+    priority: 'high',
     points: 5,
   },
   {
-    title: 'Competitor analysis',
     id: 'BUS-2',
+    title: 'Competitor analysis',
     status: 'done',
+    priority: 'medium',
     points: 0,
   },
   {
-    title: 'Develop Business Strategy',
     id: 'BUS-3',
+    title: 'Develop Business Strategy',
     status: 'todo',
+    priority: 'high',
     points: 8,
   },
   {
-    title: 'Identify potential partners',
     id: 'BUS-4',
+    title: 'Identify potential partners',
     status: 'todo',
+    priority: 'medium',
     points: 5,
   },
   {
-    title: 'Negotiate partnerships',
     id: 'BUS-5',
+    title: 'Negotiate partnerships',
     status: 'todo',
+    priority: 'low',
     points: 3,
   },
   {
-    title: 'Develop marketing plane',
     id: 'BUS-6',
+    title: 'Develop marketing plane',
     status: 'todo',
+    priority: 'high',
     points: 5,
   },
   {
-    title: 'Implement marketing plane',
     id: 'BUS-7',
+    title: 'Implement marketing plane',
     status: 'in-progress',
+    priority: 'high',
     points: 8,
   },
   {
-    title: 'Evaluate business performance',
     id: 'BUS-8',
+    title: 'Evaluate business performance',
     status: 'in-progress',
+    priority: 'medium',
     points: 5,
   },
 ];
